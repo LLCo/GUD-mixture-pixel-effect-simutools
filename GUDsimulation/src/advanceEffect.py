b8fd0e455029d33020fcd49e87d12c3df85da4f0
LLCo <543476459@qq.com>
2018-01-15 23:26:15 +0800

    two pic

--- GUDsimulation/src/advanceEffect.py
+++ GUDsimulation/src/advanceEffect.py
@@ -484,12 +484,40 @@ def advanceChangeDetection_line_show():
     print(GUDs[:, :, 0])
     GUDs = GUDs / 10
 
+    # plt.figure()
+    # plt.plot(shiftDays + 110, lw=4, label='veg A')
+    # plt.plot(np.zeros(11) + 110, lw=4, label='veg B')
+    # plt.plot((shiftDays + np.zeros(11))*0.25 + 110, lw=2, label='25%', color='r')
+    # plt.plot(GUDs[0, :, 0], ls='--', label='fa = 25%', marker='o', color='r')
+    # plt.plot((shiftDays + np.zeros(11)) * 0.5 + 110, lw=2, label='50%', color='g')
+    # plt.plot(GUDs[1, :, 0], ls='--', label='fa = 50%', marker='^', color='g')
+    # plt.plot((shiftDays + np.zeros(11)) * 0.75 + 110, lw=2, label='75%', color='b')
+    # plt.plot(GUDs[2, :, 0], ls='--', label='fa = 75%', marker='v', color='b')
+    # plt.xlabel("Time")
+    # plt.ylabel("GUD")
+    # plt.title("GUD change with Time (curvature)")
+    # plt.xticks([0, 10], ['start', 'end'])
+    # plt.legend()
+    # plt.show()
+    #
+    # plt.figure()
+    # plt.plot(shiftDays + 110, lw=4, label='veg A')
+    # plt.plot(np.zeros(11) + 110, lw=4, label='veg B')
+    # plt.plot(GUDs[0, :, 1], ls='--', label='fa = 25%', marker='o')
+    # plt.plot(GUDs[1, :, 1], ls='--', label='fa = 50%', marker='^')
+    # plt.plot(GUDs[2, :, 1], ls='--', label='fa = 75%', marker='v')
+    # plt.xlabel("Time")
+    # plt.ylabel("GUD")
+    # plt.title("GUD change with Time (threshold)")
+    # plt.xticks([0, 10], ['start', 'end'])
+    # plt.legend()
+    # plt.show()
+
     plt.figure()
     plt.plot(shiftDays + 110, lw=4, label='veg A')
     plt.plot(np.zeros(11) + 110, lw=4, label='veg B')
-    plt.plot(GUDs[0, :, 0], ls='--', label='fa = 25%', marker='o')
-    plt.plot(GUDs[1, :, 0], ls='--', label='fa = 50%', marker='^')
-    plt.plot(GUDs[2, :, 0], ls='--', label='fa = 75%', marker='v')
+    plt.plot((shiftDays + np.zeros(11)) * 0.5 + 110, lw=2, label='50%', color='g')
+    plt.plot(GUDs[1, :, 0], ls='--', label='fa = 50%', marker='^', color='g')
     plt.xlabel("Time")
     plt.ylabel("GUD")
     plt.title("GUD change with Time (curvature)")
@@ -497,23 +525,106 @@ def advanceChangeDetection_line_show():
     plt.legend()
     plt.show()
 
+
+def advanceChangeDetection_faline_show():
+    AlineP = batchCalculator.initialAParameterList
+    BlineP = batchCalculator.initialBParameterList
+    Bline = timeseris.get_initial_line(BlineP[0], BlineP[1], BlineP[2], BlineP[3], BlineP[4], BlineP[5], 3660)
+    Aline = timeseris.get_initial_line(AlineP[0], AlineP[1], AlineP[2], AlineP[3], AlineP[4], AlineP[5], 3660)
+    N = 11
+    Alines = np.zeros((3660, N))
+    shiftDays = np.linspace(-20, 20, N)
+    print(shiftDays)
+    shiftDaysUp = AlineP[0] + shiftDays * -AlineP[1] * 10
+    shiftDaysDown = AlineP[4] + shiftDays * -AlineP[5] * 10
+    for i in range(N):
+        Alines[:, i] = timeseris.get_initial_line(shiftDaysUp[i], AlineP[1], AlineP[2], AlineP[3], shiftDaysDown[i],
+                                                  AlineP[5], 3660)
+    thre = 0.09
+    GUDs = np.zeros((11, 11, 2))
+    for i, fa in enumerate(np.linspace(0, 1, 11)):
+        for j in range(N):
+            line = Alines[:, j]
+            regress_line_up, regress_line_down, totalLine = logsticFit.curve_fit(line*fa + Aline*(1-fa))  # 获取到了拟合后的像素
+            [GUDs[i, j, 0], derivative, derivative2, derivative3] = GUDcaculate(regress_line_up)
+            [GUDs[i, j, 1], threValue] = GUDThreCaculate(line*fa + Aline*(1-fa), thre)
+    print(GUDs[:, :, 0])
+    GUDs = GUDs / 10
+
     plt.figure()
-    plt.plot(shiftDays + 110, lw=4, label='veg A')
-    plt.plot(np.zeros(11) + 110, lw=4, label='veg B')
-    plt.plot(GUDs[0, :, 1], ls='--', label='fa = 25%', marker='o')
-    plt.plot(GUDs[1, :, 1], ls='--', label='fa = 50%', marker='^')
-    plt.plot(GUDs[2, :, 1], ls='--', label='fa = 75%', marker='v')
-    plt.xlabel("Time")
-    plt.ylabel("GUD")
-    plt.title("GUD change with Time (threshold)")
+    plt.plot(GUDs[:, 0, 0], ls='--', marker='o', label="RS detection ( deltaGUD = -20)")
+    plt.plot(GUDs[:, -1, 0], ls='--', marker='^', label="RS detection ( deltaGUD = 20)")
+    plt.plot([0, 10], [GUDs[0, 0, 0], GUDs[-1, 0, 0]], label="field detection ( deltaGUD = -20)")
+    plt.plot([0, 10], [GUDs[0, -1, 0], GUDs[-1, -1, 0]], label="field detection ( deltaGUD = 20)")
     plt.xticks([0, 10], ['start', 'end'])
+    plt.xlabel("fa change from 0% - 100%")
+    plt.ylabel("GUD")
+    plt.title("GUD change with fa")
     plt.legend()
     plt.show()
+
+    plt.figure(figsize=(10, 8))
+    ax2 = plt.gca()
+    im2 = ax2.imshow(GUDs[:, :, 0], interpolation='bilinear')
+    cb2 = colorbar(im2, ax=ax2)
+    cb2.ax.set_yticks([90, 100, 110, 120, 130])
+    ax2.set_title("mix GUDs (Threshold)", size=10)
+    plt.ylabel("fa")
+    plt.xlabel("interval")
+    plt.xticks(np.linspace(0, 10, 6), np.linspace(-20, 20, 6), rotation=45)
+    plt.yticks(np.linspace(0, 10, 6), ['0%', '20%', '40%', '60%', '80%', '100%'], rotation=45)
+    plt.show()
+
+    GUDsField = np.zeros((11, 11))
+    for i, fa in enumerate(np.linspace(0, 1, 11)):
+        for j, shiftday in enumerate(shiftDays):
+            GUDsField[i, j] = fa * (110 + shiftday) + (1-fa) * 110
+
+    plt.figure(figsize=(10, 8))
+    ax2 = plt.gca()
+    im2 = ax2.imshow(GUDsField, interpolation='bilinear')
+    cb2 = colorbar(im2, ax=ax2)
+    cb2.ax.set_yticks([90, 100, 110, 120, 130])
+    ax2.set_title("Field mix GUDs (Threshold)", size=10)
+    plt.ylabel("fa")
+    plt.xlabel("interval")
+    plt.xticks(np.linspace(0, 10, 6), np.linspace(-20, 20, 6), rotation=45)
+    plt.yticks(np.linspace(0, 10, 6), ['0%', '20%', '40%', '60%', '80%', '100%'], rotation=45)
+    plt.show()
+
+    intervalChangeGUDs = (GUDs[:, 1:, 0] - GUDs[:, 0:-1, 0])-(GUDsField[:, 1:] - GUDsField[:, 0:-1])
+    plt.figure(figsize=(10, 8))
+    ax2 = plt.gca()
+    im2 = ax2.imshow(intervalChangeGUDs)
+    cb2 = colorbar(im2, ax=ax2)
+    # cb2.ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
+    ax2.set_title("Change rate (Threshold)", size=10)
+    plt.ylabel("fa")
+    plt.xlabel("the change rate in each fa")
+    plt.xticks([0, 9], ['start', 'end'], rotation=45)
+    plt.yticks(np.linspace(0, 10, 6), ['0%', '20%', '40%', '60%', '80%', '100%'], rotation=45)
+    plt.show()
+
+    intervalChangeGUDs = np.abs((GUDs[1:, :, 0] - GUDs[0:-1, :, 0]))-np.abs((GUDsField[1:, :] - GUDsField[0:-1, :]))
+    intervalChangeGUDs = np.transpose(intervalChangeGUDs)
+    plt.figure(figsize=(10, 8))
+    ax2 = plt.gca()
+    im2 = ax2.imshow(intervalChangeGUDs)
+    cb2 = colorbar(im2, ax=ax2)
+    # cb2.ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
+    ax2.set_title("Change rate (Threshold)", size=10)
+    plt.ylabel("interval")
+    plt.xlabel("the change rate in each interval")
+    plt.xticks([0, 9], ['start', 'end'], rotation=45)
+    plt.yticks(np.linspace(0, 10, 6), np.linspace(-20, 20, 6), rotation=45)
+    plt.show()
+
 
 if __name__ == "__main__":
     # mixPattern()
     # GUDderive()
     # advanceReason()
     # __advanceDetection()
-    advanceChangeDetection_show_fix()
+    # advanceChangeDetection_faline_show()
+    GUDderive()
     print("end!")
